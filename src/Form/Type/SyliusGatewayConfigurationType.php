3877519916f7922e099b1640f2b95b5bb8d35605
Mipoka Henok <mipokahenok@yahoo.fr>
2023-05-08 17:21:12 +0000

    plugin

---
+++ src/Form/Type/SyliusGatewayConfigurationType.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\SyliusExamplePlugin\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class SyliusGatewayConfigurationType extends AbstractType{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('api_key', TextType::class);
+    }
+}
